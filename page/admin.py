d16989bde1be9c5c1a9e8acfaa7e0e2efa1e875b
sadagatasgarov <sedaqet.esgerov@gmail.com>
2020-11-25 15:21:56 +0400

    Page admin olusturuldu alternativ metot yotuma alindi

--- page/admin.py
+++ page/admin.py
@@ -4,4 +4,30 @@ from .models import Page
 # Register your models here.
 
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = [
+        'pk',
+        'title',
+        'created_at',
+        'updated_at',
+        'slug',
+    ]
+
+    list_filter = [
+        'status'
+    ]
+
+    search_fields = ['title']
+
+    list_display_links = ['title']
+
+
+admin.site.register(Page, PageAdmin)
+
+# @admin.register(Page)
+# class PageAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'created_at']
+#     list_display_link = ['title']
+#     class Meta:
+#         model = Page
--- page/models.py
+++ page/models.py
@@ -1,34 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 STATUS = [
-    ('draft','Taslak'),
-    ('published','Yayinlandi'),
-    ('deleted','Silindi'),
+    ('draft', 'Taslak'),
+    ('published', 'Yayinlandi'),
+    ('deleted', 'Silindi'),
 ]
 
 
 class Page(models.Model):
-    #title
+    # title
     title = models.CharField(max_length=200)
 
-    #content
+    # slug
+    #slug_text = slugify(title.replace("ı","i").replace("ə","e").replace("ş","sh"))
+    slug = models.SlugField(max_length=200, default="")
+
+    # content
     content = models.TextField()
 
-    #slug
-
-    #cover_image
-    cover_image = models.ImageField(upload_to='page')
-
-    #status
+    # cover_image
+    cover_image = models.ImageField(
+        upload_to='page',
+        null=True,
+        blank=True,
+    )
+    # status
     status = models.CharField(
         default='draft',
         choices=STATUS,
         max_length=10
     )
 
-    #created_at
+    # created_at
     created_at = models.DateTimeField(auto_now_add=True)
 
-    #updated_at
+    # updated_at
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
