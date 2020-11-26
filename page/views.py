88418822c3b307088dbf8ad6c2196fc94040d18d
sadagatasgarov <sedaqet.esgerov@gmail.com>
2020-11-26 10:42:55 +0400

    media root url ve caruselin active ozelligi

--- page/views.py
+++ page/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from page.models import Carousel
 
 # Create your views here.
 def index(request):
     context = dict()
+    context['images'] = Carousel.objects.filter(status="published")
     return render(request, 'home/index.html',context)
